281e1786ddc34241cab8af3ebaf2c1b5c71df8ee
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2022-11-15 16:52:58 -0700

    working stopwatch with OOP

--- Stopwatch OOP/main.js
+++ Stopwatch OOP/main.js
@@ -2,66 +2,51 @@ const startBtn = document.querySelector('#startTimer')
 const stopBtn = document.querySelector('#stopTimer')
 const resetBtn = document.querySelector('#resetTimer')
 const timerDisplayEl = document.querySelector('#timerDisplay')
-let milSec = 0
-let sec = 0
-let min = 0
-let hour = 0
-let int = null
-let startingTime = 0
-let deltaTime = 0
 
 class StopWatch {
     constructor(startTime = 0, endTime = 0, duration = 0, ms = 0, s = 0, m = 0, h = 0) {
         this.startTime = startTime
         this.endTime = endTime
         this.duration = duration
+        this.running = false
+        this.int = null
+        this.previousTime = 0
         this.ms = ms
         this.s = s
         this.m = m
         this.h = h
     }
     start() {
-        if (int !== null) {
-            clearInterval(int);
-        }
-        if (this.startTime === 0) {
+        if(!this.running){
             this.startTime = new Date()
-        } else {
-            console.log("Time is already running")
+            this.running = true
+            this.int = setInterval(() => this.displayTime(), 10)
         }
     }
     stop() {
-        if (this.startTime !== 0) {
-            this.endTime = new Date()
-            this.duration += (this.endTime.getTime() - this.startTime.getTime()) / 1000
-            this.startTime = 0
-        } else {
-            console.log("No time is running")
+        if(this.running) {
+            clearInterval(this.int)
+            this.previousTime += new Date().getTime() - this.startTime.getTime()
+            this.running = false
         }
     }
     reset() {
         this.startTime = 0
         this.endTime = 0
         this.duration = 0
-        clearInterval(int);
-        [milSec, sec, min, hour] = [0, 0, 0, 0]
+        this.previousTime = 0
+        this.running = false
+        clearInterval(this.int)
         timerDisplayEl.innerHTML = '00 : 00 : 00 : 000'
     }
     displayTime() {
-        let now = new Date()
+        let nowTime = new Date()
         let currentTime = 0
-        
-        currentTime = now.getTime() - startingTime.getTime()
-        console.log(currentTime)
-        deltaTime = currentTime
+        currentTime = nowTime.getTime() - this.startTime.getTime() + this.previousTime
         let fms = currentTime
         let fs = fms / 1000
         let fm = fs / 60
         let fh = fm / 60
-        console.log(fms)
-        console.log(fs)
-        console.log(fm)
-        console.log(fh)
 
         if (fms >= 1000) {
             fms = fms % 1000
@@ -72,7 +57,6 @@ class StopWatch {
         if (fm >= 60) {
             fm = fm % 60
         }
-        console.log(`0${Math.floor(fh)} : 0${Math.floor(fm)} : 0${Math.floor(fs)} : ${fms}`)
 
         this.h = fh < 10 ? "0" + Math.floor(fh) : Math.floor(fh);
         this.m = fm < 10 ? "0" + Math.floor(fm) : Math.floor(fm);
@@ -86,49 +70,12 @@ let sw = new StopWatch()
 
 startBtn.addEventListener('click', () => {
     sw.start()
-    if(!startingTime){
-        startingTime = new Date()  
-    }
-    int = setInterval(sw.displayTime, 10)
 })
 
 stopBtn.addEventListener('click', () => {
     sw.stop()
-    clearInterval(int)
-    console.log(sw.duration)
 })
 
 resetBtn.addEventListener('click', () => {
     sw.reset()
-    currentTime = 0
-    deltaTime = 0
-    startingTime = null
 })
-
-
-function figureOutTime(mils) {
-    let fms = mils
-    let fs = fms / 1000
-    let fm = fs / 60
-    let fh = fm / 60
-    console.log(fms)
-    console.log(fs)
-    console.log(fm)
-    console.log(fh)
-
-    if (fms >= 1000) {
-        fms = fms % 1000
-    }
-    if (fs >= 60) {
-        fs = fs % 60
-    }
-    if (fm >= 60) {
-        fm = fm % 60
-    }
-    console.log(`0${Math.floor(fh)} : 0${Math.floor(fm)} : 0${Math.floor(fs)} : ${fms}`)
-}
-
-figureOutTime(157)
-figureOutTime(70956)
-figureOutTime(543785)
-figureOutTime(98265432)
